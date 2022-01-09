38ab9ad92316eac62537bcc61348469212fc41a5
Alexander Ignatev <9379864-nomnoms12@users.noreply.gitlab.com>
2022-01-09 19:11:50 +0000

    Remove unused namespace

--- websocket_client_async_ssl.cpp
+++ websocket_client_async_ssl.cpp
@@ -10,7 +10,6 @@
 #include <memory>
 #include <string>
 
-namespace certify =  boost::certify;
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace websocket = beast::websocket;
