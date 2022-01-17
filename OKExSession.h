b655cfeb6ae612792bbc178b43b08624348674ec
nomnoms12 <alexander.ign0918@gmail.com>
2022-01-18 00:03:10 +0300

    Remove excess thread, improve class constructor

--- OKExSession.h
+++ OKExSession.h
@@ -6,6 +6,7 @@
 #include <boost/asio/strand.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/ssl.hpp>
+#include <utility>
 #include <boost/beast/websocket.hpp>
 #include <boost/certify/extensions.hpp>
 #include <boost/certify/https_verification.hpp>
@@ -19,77 +20,73 @@ using tcp = boost::asio::ip::tcp;
 namespace certify = boost::certify;
 namespace json = boost::json;
 
-void fail(beast::error_code ec, char const* what)
-{
-    std::cerr << what << ": " << ec.message() << std::endl;
-}
+auto const HOST = "wspap.okex.com";
+auto const PORT = "8443";
+auto const TARGET = "/ws/v5/public?brokerId=9999";
 
 class OKExSession : public std::enable_shared_from_this<OKExSession>
 {
-    websocket::stream<beast::ssl_stream<beast::tcp_stream>> ws;
+    std::shared_ptr<websocket::stream<beast::ssl_stream<beast::tcp_stream>>> ws;
     beast::flat_buffer buffer;
-    std::function<void (std::string)> callback;
+    std::function<void (std::string)> handler;
 
     void on_read(beast::error_code ec, std::size_t bytes_transferred)
     {
         boost::ignore_unused(bytes_transferred);
 
         if(ec)
-            return fail(ec, "read");
+            throw std::runtime_error("on_read: " + ec.message());
 
-        callback(beast::buffers_to_string(buffer.data()));
+        handler(beast::buffers_to_string(buffer.data()));
 
         buffer.clear();
-        ws.async_read(buffer, beast::bind_front_handler(&OKExSession::on_read, shared_from_this()));
+        ws->async_read(buffer, beast::bind_front_handler(&OKExSession::on_read, shared_from_this()));
     };
 
 public:
-    explicit OKExSession(net::io_context& ioc, ssl::context& ctx, std::function<void (std::string)> callback)
-        : ws(net::make_strand(ioc), ctx),
-          callback(std::move(callback))
+    explicit OKExSession(net::io_context& ioc, std::function<void (std::string)> handler)
+        : handler(std::move(handler))
     {
-        std::string host = "wspap.okex.com";
-        std::string port = "8443";
-        std::string target = "/ws/v5/public?brokerId=9999";
+        ssl::context ctx{ ssl::context::tls_client };
+        ctx.set_verify_mode(ssl::context::verify_peer | ssl::context::verify_fail_if_no_peer_cert);
+        ctx.set_default_verify_paths();
+        certify::enable_native_https_server_verification(ctx);
+
+        ws = std::make_shared<websocket::stream<beast::ssl_stream<beast::tcp_stream>>>(ioc, ctx);
+
+        std::string host(HOST);
 
         tcp::resolver resolver{ioc};
-        auto results = resolver.resolve(host, port);
-        auto ep = beast::get_lowest_layer(ws).connect(results);
+        auto results = resolver.resolve(host, PORT);
+        auto ep = beast::get_lowest_layer(*ws).connect(results);
 
-        SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str());
+        SSL_set_tlsext_host_name(ws->next_layer().native_handle(), host.c_str());
         host += ':' + std::to_string(ep.port());
 
-        ws.next_layer().handshake(ssl::stream_base::client);
+        ws->next_layer().handshake(ssl::stream_base::client);
 
-        beast::get_lowest_layer(ws).expires_never();
-        ws.set_option(websocket::stream_base::timeout::suggested(beast::role_type::client));
+        beast::get_lowest_layer(*ws).expires_never();
+        ws->set_option(websocket::stream_base::timeout::suggested(beast::role_type::client));
 
-        ws.set_option(websocket::stream_base::decorator(
+        ws->set_option(websocket::stream_base::decorator(
             [](websocket::request_type& req)
             {
                 req.set("x-simulated-trading", "1");
             }
         ));
 
-        ws.handshake(host, target);
-
-        json::value jv = {
-            { "op", "subscribe" },
-            { "args", {
-                {
-                    { "channel", "tickers" },
-                    { "instId", "BTC-USDT" },
-                }
-            }}
-        };
-
-        ws.write(net::buffer(json::serialize(jv)));
+        ws->handshake(host, TARGET);
     }
 
-    void run()
+    void write(std::string message)
     {
-        ws.async_read(buffer, beast::bind_front_handler(&OKExSession::on_read, shared_from_this()));
-    };
+        ws->write(net::buffer(message));
+    }
+
+    void async_read()
+    {
+        ws->async_read(buffer, beast::bind_front_handler(&OKExSession::on_read, shared_from_this()));
+    }
 };
 
 #endif  // OKEX_GATEWAY_OKEXSESSION_H
--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 std::atomic<bool> running(true);
 std::shared_ptr<Publisher> gateway;
 std::shared_ptr<Subscriber> core;
-std::shared_ptr<aeron::SleepingIdleStrategy> idle_strategy;
 
 void okex_callback(const std::string& message)
 {
@@ -33,30 +32,32 @@ void sigint_handler(int)
 int main()
 {
     net::io_context ioc;
-
-    ssl::context ctx{ ssl::context::tls_client };
-    ctx.set_verify_mode(ssl::context::verify_peer | ssl::context::verify_fail_if_no_peer_cert);
-    ctx.set_default_verify_paths();
-    certify::enable_native_https_server_verification(ctx);
-
-    std::make_shared<OKExSession>(ioc, ctx, okex_callback)->run();
-
-    std::thread([&]() {
-        ioc.run();
-    }).detach();
+    std::shared_ptr<OKExSession> okex = std::make_shared<OKExSession>(ioc, okex_callback);
 
     gateway = std::make_shared<Publisher>("aeron:ipc", 1001);
     core = std::make_shared<Subscriber>(core_callback, "aeron:ipc", 1002);
 
-    idle_strategy = std::make_shared<aeron::SleepingIdleStrategy>(std::chrono::duration<long, std::milli>(100));
-
     gateway->connect();
     core->connect();
+
+    json::value jv = {
+        { "op", "subscribe" },
+        { "args", {
+            {
+                { "channel", "tickers" },
+                { "instId", "BTC-USDT" },
+            }
+        }}
+    };
+
+    okex->write(json::serialize(jv));
+    okex->async_read();
 
     signal(SIGINT, sigint_handler);
     while (running)
     {
-        idle_strategy->idle(core->poll());
+        ioc.run_for(std::chrono::milliseconds(100));
+        core->poll();
     }
 
     return EXIT_SUCCESS;
